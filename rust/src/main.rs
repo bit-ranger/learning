6279c481106c930a8f322b9b62a051b76061735a
bit-ranger <sincerebravefight@gmail.com>
2019-10-24 19:18:05 +0800

    guess loop

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,20 +4,25 @@ use std::io;
 use rand::Rng;
 
 fn main() {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
     let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+    println!("The secret number is: {}", secret_number);
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed: {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
