347950e4651c3a69680be94df4cd6b7431929356
bit-ranger <sincerebravefight@gmail.com>
2019-10-24 20:15:11 +0800

    guess full

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,21 +4,25 @@ use std::io;
 use rand::Rng;
 
 fn main() {
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("Guess the number!");
 
-    println!("The secret number is: {}", secret_number);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
     loop {
         println!("Please input your guess.");
 
         let mut guess = String::new();
+
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
+
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
+
+        println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
