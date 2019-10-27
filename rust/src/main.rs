7432092e3aa37fb6fff4bca35903ad189fbdf88d
bit-ranger <sincerebravefight@gmail.com>
2019-10-27 10:12:39 +0800

    slice

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,36 +1,32 @@
-use std::cmp::Ordering;
-use std::io;
 
-use rand::Rng;
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
 
-fn main() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
         }
     }
+
+    &s[..]
+}
+
+
+fn main() {
+    let my_string = String::from("hello world");
+
+    // first_word 中传入 `String` 的 slice
+    let word = first_word(&my_string[..]);
+    println!("{}", word);
+
+    let my_string_literal = "hello world";
+
+    // first_word 中传入字符串字面值的 slice
+    let word = first_word(&my_string_literal[..]);
+    println!("{}", word);
+
+    // 因为字符串字面值 **就是** 字符串 slice，
+    // 这样写也可以，即不使用 slice 语法！
+    let word = first_word(my_string_literal);
+    println!("{}", word);
 }
