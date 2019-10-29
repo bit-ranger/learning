f2ed0b14e71a00bb2e34c71356153d4e2f7f86bd
bit-ranger <sincerebravefight@gmail.com>
2019-10-29 17:09:53 +0800

    generic

---
+++ rust/generic/src/demo1.rs
@@ -0,0 +1,23 @@
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+pub fn demo() {
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+
+    println!("p.x = {}", float.x());
+}
