6571e3e3316bb253821c266e1c2beebb3b926619
bit-ranger <sincerebravefight@gmail.com>
2019-10-31 17:32:06 +0800

    box

---
+++ rust/boxdemo/src/lib.rs
@@ -0,0 +1,19 @@
+#[cfg(test)]
+mod tests {
+    use List::{Cons, Nil};
+
+    #[test]
+    fn it_works() {
+        let b = Box::new(5);
+        println!("b = {}", b);
+    }
+
+    enum List {
+        Cons(i32, Box<List>),
+        Nil,
+    }
+
+    fn it_works_2() {
+        let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    }
+}
