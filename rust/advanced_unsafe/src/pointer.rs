48037b537b42cb4d9e09db17e5d287edc9e42916
bit-ranger <sincerebravefight@gmail.com>
2019-11-01 19:49:50 +0800

    unsafe pointer

---
+++ rust/advanced_unsafe/src/pointer.rs
@@ -0,0 +1,20 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn work() {
+        let mut num = 5;
+
+        let r1 = &num as *const i32;
+        let r2 = &mut num as *mut i32;
+
+        unsafe {
+            println!("r1 is: {}", *r1);
+            println!("r2 is: {}", *r2);
+        }
+
+        let address = 0b00011000usize;
+        let r = address as *const i32;
+    }
+}
