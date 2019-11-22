6b1e9f36dfdcf2a8ea6cbe284a9d79e3616fc117
bit-ranger <sincerebravefight@gmail.com>
2019-11-22 14:33:02 +0800

    linux env log

--- linux/env/src/bin/log.rs
+++ linux/env/src/bin/log.rs
@@ -1,11 +1,12 @@
-use libc::{syslog, openlog, LOG_PID, LOG_CONS, LOG_USER, LOG_INFO, getpid};
+use libc::{syslog, openlog, LOG_PID, LOG_CONS, LOG_USER, LOG_INFO, getpid, closelog};
 use std::os::raw::c_char;
 
 fn main() {
 
     unsafe {
         openlog("log_demo".as_ptr() as *const c_char, LOG_PID|LOG_CONS, LOG_USER);
-        syslog(LOG_INFO, "pid = %d".as_ptr() as *const c_char, getpid())
+        syslog(LOG_INFO, "pid = %d".as_ptr() as *const c_char, getpid());
+        closelog();
     }
 
 }
