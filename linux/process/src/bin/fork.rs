2834c463c49108a4e4c9e53760786f6014749be6
bit-ranger <sincerebravefight@gmail.com>
2019-11-25 19:03:27 +0800

    linux process waitpid

--- linux/process/src/bin/fork.rs
+++ linux/process/src/bin/fork.rs
@@ -1,5 +1,8 @@
-use nix::unistd::{fork, getpid};
+use nix::unistd::{fork, getpid, sleep};
 use nix::unistd::ForkResult::{self, Parent, Child};
+use nix::sys::wait::{wait, waitpid};
+use nix::sys::wait::WaitStatus::Exited;
+use nix::sys::wait::WaitPidFlag;
 
 fn main() {
 
@@ -9,15 +12,25 @@ fn main() {
             match fr {
                 Child => {
                     for i in 0..5{
-                        println!("{}, I am a child", getpid())
+                        println!("{}, I am a child", getpid());
+                        sleep(1);
                     }
 
                 },
                 Parent{child} => {
-                    for i in 0..5{
-                        println!("{}, I am a parent, child {}", getpid(), child)
+//                    match wait().unwrap(){
+                    match waitpid(child, Some(WaitPidFlag::WNOHANG)).unwrap(){
+                        Exited(child_pid, child_exit_code) => {
+                            println!("{}, child exit {}, {}", getpid(), child_pid, child_exit_code)
+                        }
+                        _ => {
+                            println!("not exited")
+                        }
                     }
-
+                    for i in 0..5{
+                        println!("{}, I am a parent, child {}", getpid(), child);
+                        sleep(1);
+                    }
                 }
             }
         },
