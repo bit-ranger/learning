79d33077b883c1a13080c41d9bdad9f3742657aa
bit-ranger <sincerebravefight@gmail.com>
2019-11-26 17:09:25 +0800

    linux process pipe

--- linux/process/src/bin/pipe.c
+++ linux/process/src/bin/pipe.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int main()
+{
     int data_processed;
     int file_pipes[2];
     const char some_data[] = "123";
@@ -14,25 +15,20 @@ int main() {
 
     if (pipe(file_pipes) == 0) {
         fork_result = fork();
-        if (fork_result == -1) {
+        if (fork_result == (pid_t)-1) {
             fprintf(stderr, "Fork failure");
             exit(EXIT_FAILURE);
         }
 
-// We've made sure the fork worked, so if fork_result equals zero, we're in the child process.
-
         if (fork_result == 0) {
-            data_processed = read(file_pipes[0], buffer, BUFSIZ);
-            printf("Read %d bytes: %s\n", data_processed, buffer);
-            exit(EXIT_SUCCESS);
+            sprintf(buffer, "%d", file_pipes[0]);
+            (void)execl("pipe4", "pipe4", buffer, (char *)0);
+            exit(EXIT_FAILURE);
         }
-
-// Otherwise, we must be the parent process.
-
         else {
             data_processed = write(file_pipes[1], some_data,
                                    strlen(some_data));
-            printf("Wrote %d bytes\n", data_processed);
+            printf("%d - wrote %d bytes\n", getpid(), data_processed);
         }
     }
     exit(EXIT_SUCCESS);
