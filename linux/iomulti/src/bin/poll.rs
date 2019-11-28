78bd7ed2a4d7b79dbbe5b291fc29a20e236c83c2
bit-ranger <sincerebravefight@gmail.com>
2019-11-28 16:23:08 +0800

    linux iomulti poll

--- linux/iomulti/src/bin/poll.rs
+++ linux/iomulti/src/bin/poll.rs
@@ -1,6 +1,25 @@
-use nix::poll::poll;
+use nix::poll::{poll, PollFd, PollFlags};
+use nix::unistd::read;
 
 fn main() {
 
-    poll()
+    let file = 0;
+
+    let fds = [PollFd::new(file, PollFlags::POLLIN)];
+
+    loop{
+        let mut fds_act = fds;
+        poll(&mut fds_act, 100);
+
+        for fd in fds_act.iter(){
+            if let Some(flag) =  fd.revents(){
+                if flag & PollFlags::POLLIN == PollFlags::POLLIN{
+                    let mut buffer = [0u8;128];
+                    let nread = read(file, &mut buffer).unwrap();
+                    println!("{}", unsafe { String::from_raw_parts(buffer.as_mut_ptr(), nread, nread) });
+                }
+
+            }
+        }
+    }
 }
