2e2f91e506901e043feebde53352aff2781c9d84
bit-ranger <sincerebravefight@gmail.com>
2019-11-28 17:23:35 +0800

    linux iomulti epoll

--- linux/iomulti/src/bin/epoll.rs
+++ linux/iomulti/src/bin/epoll.rs
@@ -1,26 +1,29 @@
 use nix::sys::epoll::{epoll_create1, epoll_ctl, epoll_wait, EpollCreateFlags, EpollOp, EpollEvent, EpollFlags};
 use std::mem::MaybeUninit;
 use nix::unistd::read;
+use nix::Error;
 
 fn main() {
+    real_main().unwrap();
+}
 
-    let efd = epoll_create1(EpollCreateFlags::EPOLL_CLOEXEC).unwrap();
+fn real_main() -> Result<usize, Error>{
+    let efd = epoll_create1(EpollCreateFlags::EPOLL_CLOEXEC)?;
 
     let fd = 0;
     let mut event_watch = EpollEvent::new(EpollFlags::EPOLLIN, fd);
-    epoll_ctl(efd, EpollOp::EpollCtlAdd, fd as i32, Some(&mut event_watch)).unwrap();
+    epoll_ctl(efd, EpollOp::EpollCtlAdd, fd as i32, Some(&mut event_watch))?;
 
     let mut events_active:[EpollEvent;1] = unsafe { MaybeUninit::uninit().assume_init() };
-    let epoll_n = epoll_wait(efd, &mut events_active, -1).unwrap();
+    let epoll_n = epoll_wait(efd, &mut events_active, -1)?;
 
     for i in 0..epoll_n {
         let eve = events_active[i];
         let fd = eve.data();
         let mut buffer = [0u8;128];
-        let nread = read(fd as i32, &mut buffer).unwrap();
+        let nread = read(fd as i32, &mut buffer)?;
         println!("{}", unsafe { String::from_raw_parts(buffer.as_mut_ptr(), nread, nread) });
     }
 
-
-
+    return Result::Ok(0);
 }
