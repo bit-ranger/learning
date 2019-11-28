ec2ef3d3e145d9cac7e31f9e5cb3574fca2b1f41
bit-ranger <sincerebravefight@gmail.com>
2019-11-28 17:11:17 +0800

    linux iomulti epoll

---
+++ linux/iomulti/src/bin/epoll.rs
@@ -0,0 +1,26 @@
+use nix::sys::epoll::{epoll_create1, epoll_ctl, epoll_wait, EpollCreateFlags, EpollOp, EpollEvent, EpollFlags};
+use std::mem::MaybeUninit;
+use nix::unistd::read;
+
+fn main() {
+
+    let efd = epoll_create1(EpollCreateFlags::EPOLL_CLOEXEC).unwrap();
+
+    let fd = 0;
+    let mut event_watch = EpollEvent::new(EpollFlags::EPOLLIN, fd);
+    epoll_ctl(efd, EpollOp::EpollCtlAdd, fd as i32, Some(&mut event_watch)).unwrap();
+
+    let mut events_active:[EpollEvent;1] = unsafe { MaybeUninit::uninit().assume_init() };
+    let epoll_n = epoll_wait(efd, &mut events_active, -1).unwrap();
+
+    for i in 0..epoll_n {
+        let eve = events_active[i];
+        let file = eve.data();
+        let mut buffer = [0u8;128];
+        let nread = read(file as i32, &mut buffer).unwrap();
+        println!("{}", unsafe { String::from_raw_parts(buffer.as_mut_ptr(), nread, nread) });
+    }
+
+
+
+}
