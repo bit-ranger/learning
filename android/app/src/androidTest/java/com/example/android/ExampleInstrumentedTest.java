db4bece9704eae30514dcc149a0bbd0056ea6891
bit-ranger <sincerebravefight@gmail.com>
2020-03-25 17:02:00 +0800

    feat(android): basic

--- android/app/src/androidTest/java/com/example/android/ExampleInstrumentedTest.java
+++ android/app/src/androidTest/java/com/example/android/ExampleInstrumentedTest.java
@@ -1,12 +1,13 @@
-package com.example.myapplication;
+package com.example.android;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -18,8 +19,8 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.myapplication", appContext.getPackageName());
+        assertEquals("com.example.android", appContext.getPackageName());
     }
 }
