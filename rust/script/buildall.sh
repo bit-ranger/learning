44acc200bb1148a059640b3cc1a72b028a157bbc
bit-ranger <sincerebravefight@gmail.com>
2020-07-20 11:57:14 +0800

    build(rust): cross platform

--- rust/script/buildall.sh
+++ rust/script/buildall.sh
@@ -13,5 +13,5 @@ docker run --rm -u $(id -u):$(id -g) -v $(pwd):/workdir rust-crossbuild /usr/loc
 docker run --rm -u $(id -u):$(id -g) -v $(pwd):/workdir -e CROSS_TRIPLE=x86_64-w64-mingw32 rust-crossbuild /usr/local/rust/bin/cargo build --target=x86_64-pc-windows-gnu  --release
 
 # macos
-docker run --rm -u $(id -u):$(id -g) -v $(pwd):/workdir -e RUSTFLAGS="-C linker=/workdir/macbuild.sh" -e CROSS_TRIPLE=x86_64-apple-darwin rust-crossbuild /usr/local/rust/bin/cargo build --target=x86_64-apple-darwin  --release
+docker run --rm -u $(id -u):$(id -g) -v $(pwd):/workdir -e RUSTFLAGS="-C linker=/workdir/script/macbuild.sh" -e CROSS_TRIPLE=x86_64-apple-darwin rust-crossbuild /usr/local/rust/bin/cargo build --target=x86_64-apple-darwin  --release
 
